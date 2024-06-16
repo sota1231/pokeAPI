df007dacb9006c73cc5807bff85982b796bc6071
sota1231 <bacchuszzzzzzyyyyyy@gmail.com>
2024-06-16 10:37:20 +0900

    説明書き

--- src/App.js
+++ src/App.js
@@ -17,17 +17,21 @@ function App() {
     const fetchPokemonData = async () => {
       // すべてのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
+
       // 各ポケモンの詳細なデータを取得
       loadPokemon(res.results);
-      console.log(res);
+
+      // 状態変数の更新
       setNextURL(res.next);
       setPrevURL(res.previous)
       setLoading(false);
     };
+    // 上記の関数を実行
     fetchPokemonData();
   }, []);
 
-  // 取得したデータをさらに展開（ネストが深い）
+  // 上記の関数内の関数
+  // 取得したデータをさらに展開（２回目）
   const loadPokemon = async (data) => {
     let _pokemonData = await Promise.all(
       data.map((pokemon) => {
@@ -37,21 +41,29 @@ function App() {
         return pokemonRecord;
       })
     );
+    
+    // _pokemonDataをpokemonデータに格納して関数外でも使えるようにする
+    // 最後表示するときに使う
     setPokemonData(_pokemonData);
   };
-  // console.log(pokemonData);
 
+  // 次へボタン
   const handleNextPage = async () => {
     setLoading(true);
+
+    // すべてのポケモンデータを取得し、詳細データを取得
     let data = await getAllPokemon(nextURL);
     await loadPokemon(data.results);
+
+    // 状態変数を更新
     setNextURL(data.next);
     setPrevURL(data.previpus);
     setLoading(false);
   }
+
+  // 前へボタン
   const handlePrevPage = async () => {
     if(!prevURL) return;
-
     setLoading(true);
     let data = await getAllPokemon(prevURL);
     await loadPokemon(data.results);
@@ -64,11 +76,14 @@ function App() {
     <>
       <Navbar />
       <div className="App">
+
+        {/* loadingがtrueの時ロード中を表示 */}
         {loading ? (
           <h1>ロード中・・・</h1>
         ) : (
           <>
             <div className="pokemonCardContainer">
+              {/* useEffectで取得した詳細データを状態変数に格納してここでさらにmapで詳細データを取得する */}
               {pokemonData.map((pokemon, i) => {
                 return <Card key={i} pokemon={pokemon} />;
               })}
--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -1,12 +1,17 @@
+
+
+// ポケモンのデータをjson形式にして返すメソッド
 export const getAllPokemon = (url) => {
   return new Promise((resolve, reject) => {
-    //
+    // fetchを行うことでurl(パス)先のデータを取得することができる（多分）
+    // そのデータをjson形式にしてdataという名前で返す
     fetch(url)
       .then((res) => res.json())
       .then((data) => resolve(data));
   });
 };
 
+// 上記で取得したデータをmapで回してその中に格納されているURLをさらにjsonにしてdataにして返す工程
 export const getPokemon = (url) => {
   return new Promise((resolve, reject) => {
     //promiseチェーンのthenでつなげる
